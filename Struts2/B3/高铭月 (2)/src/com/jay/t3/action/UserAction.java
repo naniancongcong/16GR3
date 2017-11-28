a8d6f780c21a4e1c0f6404e8027690eecb98b9cd
congcongnanian <2195480636@qq.com>
2017-11-28 20:06:14 +0800

    高铭月TT—03

---
+++ Struts2/B3/高铭月 (2)/src/com/jay/t3/action/UserAction.java
@@ -0,0 +1,13 @@
+package com.jay.t3.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class UserAction implements Action {
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
